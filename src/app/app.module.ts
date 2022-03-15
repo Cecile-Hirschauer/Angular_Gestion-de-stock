38355b13ed290b967d327c8b6bfb88a7387792d3
Cécile HIRSCHAUER <cecile.hirschauer@suricats-consulting.com>
2022-03-15 15:34:17 +0100

    Create register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageLoginComponent
+    PageLoginComponent,
+    PageInscriptionComponent
   ],
   imports: [
     BrowserModule,
